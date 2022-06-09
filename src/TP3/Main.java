3caa6df358270d4f784c53498cf9a1121120b73a
Sylvain M <syl.migeon@gmail.com>
2022-06-09 12:27:57 +0200

    Changes by qvngu

--- src/TP3/Main.java
+++ src/TP3/Main.java
@@ -22,19 +22,48 @@ public class Main {
         }
         nombre = scanner.nextInt();
 
-        List coureurs = new ArrayList<Coureur>();
+        List<Coureur> coureurs = new ArrayList<>();
+        List<Coureur> classement = new ArrayList<>();
 
         for (int i = 0; i < nombre; i++) {
             System.out.println("Nom du coureur #" + (i + 1) + " : ");
-            coureurs.add(scanner.next());
+            coureurs.add(new Coureur(scanner.next()));
         }
 
+        //Arrêt Programme
+        String com = "";
 
-        boolean course = true;
-
-        while(course){
-
+        while(!com.toLowerCase().equals("q")){
+            System.out.println("Que voulez-vous faire ?\n" +
+                    "q : quitter le programme\n" +
+                    "a : afficher tous les coureurs\n" +
+                    "c : afficher le classement\n" +
+                    "f : enregister une arrivée\n"
+            );
+            com = scanner.next().toLowerCase();
+            switch (com) {
+                case ("a"): // Affichage Coureurs
+                    for (Coureur coureur : coureurs) {
+                        System.out.println(coureur);
+                    }
+                    break;
+                case ("c"): // Afficher le classement
+                    for (int i = 0; i < nombre; i++) {
+                        System.out.println("Rang #" + (i+1) + " : " + ((i < classement.size())? classement.get(i) : "Coureur non arrivé"));
+                    }
+                    break;
+                case ("f"): // Enregistrer une arrivée
+                    System.out.println("Quel coureur est arrivé ? Précisez son numéro de dossard.");
+                    while(!scanner.hasNextInt()) {
+                        System.out.println("Veuillez taper un nombre!");
+                        scanner.next();
+                    }
+                    classement.add(coureurs.get(scanner.nextInt()));
+                    break;
+            }
         }
+
+        
 
     }
 }
