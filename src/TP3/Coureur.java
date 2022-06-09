9cf842bfe0c7011e5c43754e19167d637cbf9f48
QV NGUYEN <qv.nguyen00@gmail.com>
2022-06-09 10:04:26 +0200

    initial

---
+++ src/TP3/Coureur.java
@@ -0,0 +1,11 @@
+package TP3;
+
+public class Coureur {
+    private final int numDossard;
+    private final String nom;
+
+    public Coureur(final int numDossard, final String nom) {
+        this.numDossard = numDossard;
+        this.nom = nom;
+    }
+}
