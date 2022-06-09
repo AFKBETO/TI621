291557a352707196ee4d01085eee9c0109f351b1
Sylvain M <syl.migeon@gmail.com>
2022-06-09 11:23:18 +0200

    Version 2

--- src/TP3/Coureur.java
+++ src/TP3/Coureur.java
@@ -1,14 +1,48 @@
 package TP3;
 
 public class Coureur {
+    private static int COMPTEUR = 0;
     private final int numDossard;
     private final String nom;
 
-    public Coureur(final int numDossard, final String nom) {
-        if (numDossard < 1) {
-            throw new IllegalArgumentException("Numéro de dossard doit être positif!");
-        }
-        this.numDossard = numDossard;
+    private Etat etat = Etat.ENCOURS;
+    private int temps = 0;
+
+    public Coureur(final String nom) {
+        this.numDossard = ++COMPTEUR;
         this.nom = nom;
+    }
+
+    public int getTemps() {
+        return temps;
+    }
+
+    public void setTemps(int temps) {
+        this.temps = temps;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void abandonner() {
+        etat = Etat.ABANDON;
+    }
+
+    public void arriver() {
+        etat = Etat.ARRIVEE;
+    }
+
+    public void disqualifier() {
+        etat = Etat.DISQUALIF;
+    }
+
+    public Etat getEtat() {
+        return etat;
+    }
+
+    @Override
+    public String toString() {
+        return "Coureur #" + numDossard + " : " + nom + ", statut : " + etat;
     }
 }
