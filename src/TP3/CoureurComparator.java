7e09d54b2c4e7ff5da624c292d0ed38df285eb1b
QV NGUYEN <qv.nguyen00@gmail.com>
2022-06-10 09:46:03 +0200

    comparateur Coureur

---
+++ src/TP3/CoureurComparator.java
@@ -0,0 +1,22 @@
+package TP3;
+
+import java.util.Comparator;
+
+public class CoureurComparator implements Comparator<Coureur> {
+
+    @Override
+    public int compare(Coureur o1, Coureur o2) {
+        if (o1.getEtat() == o2.getEtat()) {
+            if (o1.getEtat() == Etat.ARRIVEE) {
+                if (o1.getTemps() < o2.getTemps()) {
+                    return -1;
+                } else if (o1.getTemps() > o2.getTemps()) {
+                    return 1;
+                }
+            }
+        } else {
+            
+        }
+        return 0;
+    }
+}
