0626ad943446962a1ad692ba9ef19ba93857930c
Maxence Robi <maxence.robichon@efrei.net>
2022-06-15 11:39:47 +0200

    Document constructeur,getter,setter

--- src/TP4/Document.java
+++ src/TP4/Document.java
@@ -7,4 +7,34 @@ public class Document {
     private String DocumentName;
     private Date DocumentDate;
     private String StorageAddress;
+   public Document(String documentName, Date documentDate, String storageAddress) {
+        this.DocumentName = documentName;
+        this.DocumentDate = documentDate;
+        this.StorageAddress = storageAddress;
+    }
+
+    public String getDocumentName() {
+        return DocumentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.DocumentName = documentName;
+    }
+
+    public Date getDocumentDate() {
+        return DocumentDate;
+    }
+
+    public void setDocumentDate(Date documentDate) {
+        this.DocumentDate = documentDate;
+    }
+
+    public String getStorageAddress() {
+        return StorageAddress;
+    }
+
+    public void setStorageAddress(String storageAddress) {
+        this.StorageAddress = storageAddress;
+    }
+
 }
